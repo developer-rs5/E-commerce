8b06727d6bf8819e1f89eb67b073010c01427c21
Developer RS <164728448+developer-rs5@users.noreply.github.com>
2025-07-09 19:18:34 +0530

    .

--- backend/routes/LoadrandomRoute.js
+++
@@ -1,8 +0,0 @@
-
-import express from 'express'
-import { getRandomProducts } from '../controllers/productController.js'
-const router = express.Router();
-
-router.get('/random', getRandomProducts);
-
-export default router;
